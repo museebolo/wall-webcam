acdc154cbffb3b1dc2e06492144d42f8daeb08c0
romain <rom1@canel.ch>
2020-02-19 15:38:44 +0100

    sépare la configuration. Envoie sur Telegram

--- print_webcam.sh
+++ print_webcam.sh
@@ -8,42 +8,73 @@
 # Language: Python 3
 # Description: Affichage des webcam dans le mur.
 
-URL="https://www.abcm.ch/webcam/url_list.txt"
-TMP="tmp"
-DEST=${TMP}/imglist
-DEST_TEMP=${TMP}/imglist.dl
+config_f="config.sh"
 
-wget --no-check-certificate -O "$DEST_TEMP" "$URL"
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-        exit 1
-else
-        rm "$DEST"
-        mv "$DEST_TEMP" "$DEST"
-fi
+usage()
+{
+        echo "Usage: $(basename $0)"
+}
+
+send_t_message()
+{
+        mess="${1}"
+        echo "Envoyer le message sur Telegram: ${mess}"
+        t_mess="https://api.telegram.org"
+        t_mess="${t_mess}/${T_BOT_ID}"
+	t_mess="${t_mess}/sendMessage?chat_id=${T_CHAN_ID}&text=${mess}"
+        curl "${t_mess}"
+}
+
+create_image()
+{
+	wget --no-check-certificate -O "$DEST_TEMP" "$URL"
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	else
+		rm "$DEST"
+		mv "$DEST_TEMP" "$DEST"
+	fi
 
 
-num=0
-for i in $(cat ${TMP}/imglist)
-do
-        wget -O "${TMP}/$num.jpg" --tries=1 --timeout=4 "$i"
-        num=$(($num+1))
-done
+	num=0
+	for i in $(cat ${TMP}/imglist)
+	do
+		wget -O "${TMP}/$num.jpg" --tries=1 --timeout=4 "$i"
+		num=$(($num+1))
+	done
 
 
-if [ $(find ${TMP} -size 0b -name "*.jpg"|wc -l) != "0" ]
+	if [ $(find ${TMP} -size 0b -name "*.jpg"|wc -l) != "0" ]
+	then
+		for f in $(find ${TMP} -size 0b -name "*.jpg") 
+		do
+			cp notfound.jpg $f
+			#send_t_message "Webcam: Problème avec l'image ${f}"
+		done
+	fi
+
+	for i in $(seq 0 8)
+	do 
+		convert ${TMP}/$i.jpg -resize 400x300\! ${TMP}/src-$i.jpg
+	done
+
+	montage ${TMP}/src-*.jpg -background "#ffffff" -border 0 -geometry +3+3 ${TMP}/webcams.jpg
+}
+
+# Charge la configuration
+#if [[ ! -a ${config_f} ]]
+if [ ! -f ${config_f} ]
 then
-        for f in $(find ${TMP} -size 0b -name "*.jpg") 
-        do
-                cp notfound.jpg $f
-        done
+	echo "Il manque le fichier de configuration: ${config_f}"
+	usage
+	exit 1
 fi
+source ${config_f}
 
-for i in $(seq 0 8)
-do 
-        convert ${TMP}/$i.jpg -resize 400x300\! ${TMP}/src-$i.jpg
+while true
+do
+	create_image
+	feh --bg-center ${TMP}/webcams.jpg
+	sleep ${REFRESH_DELAY}
 done
-
-montage ${TMP}/src-*.jpg -background "#ffffff" -border 0 -geometry +3+3 ${TMP}/webcams.jpg
-
-#feh -F /tmp/webcams.jpg
